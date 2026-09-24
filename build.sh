15b84a1e3966853caba8946a85540ce36917a930
SamrtinSinale <sartsine@gmail.com>
2026-09-24 19:41:03 +0800

    fix(build): drop -S superkey so FolkPatch app can auth via trusted-manager uid

--- build.sh
+++ build.sh
@@ -150,7 +150,6 @@ echo "[+] Applying FolkPatch (KernelPatch) to Image..."
 ./${KP_DIR}/kptools-linux \
     -p \
     -i out/arch/arm64/boot/Image \
-    -S "qwerqwer4321" \
     -k ${KP_DIR}/kpimg-fp \
     -o out/arch/arm64/boot/Image
 
